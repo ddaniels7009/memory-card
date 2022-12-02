7911142f2399f62f781414e1a425fd8d2a63cb30
David Daniels <ddaniels7009@gmail.com>
2022-12-01 20:18:13 -0500

    save

---
+++ src/components/sorter.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Sorter = (props) => {
+
+const [item, setItem] = useState({
+    name:'',
+    id: '1'
+
+});
+const [itemList, setItemList] = useState([]);
+
+
+}
